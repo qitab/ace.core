541666d35f1f3b0f52133d0ccf16be798af8d860
Andrzej Walczak <andrzejwalczak@google.com>
2023-08-29 10:49:21 -0700

    Allow NIL to be an empty string.
    This offloads less assumptions onto the client
    making the function more generic.
    
    PiperOrigin-RevId: 561077861

--- string.lisp
+++ string.lisp
@@ -667,7 +667,7 @@ Parameters:
 
 (defun* emptyp (string)
   "True if STRING is empty."
-  (declare (self foldable inline (string) boolean))
+  (declare (self foldable inline ((or null string)) boolean))
   (zerop (the size (length string))))
 
 (defun* search-replace (old new string &key (start 0) (end nil) (test #'char=))
